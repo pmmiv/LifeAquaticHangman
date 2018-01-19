db4d559ccca91e3cdf8fb2b64f2a978b1b3f29d0
pmmiv <pmmilamiv@gmail.com>
2018-01-19 15:09:53 -0600

    styled the page

---
+++ javascript/script.js
@@ -0,0 +1,40 @@
+// create an array of words
+// var allWords =  ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""];
+
+// once a word has been played remove it from possibilities
+
+// var remainingWords =  (allWords - wordsPlayed);
+// var wordsPlayed = wordsPlayed + wordInPlay; 
+// clearly problematic
+
+// create a way of picking one randomly
+
+
+// count the number of letters of the word and generate that number of blanks
+
+// answer[];
+// letters = wordInPlay.charAt[i]
+
+// make an array of the letters using .charAt and .push
+
+// display the number of blanks on screen when the button is pressed and the word is picked
+
+
+// display the correct letters picked in order
+
+
+// keep track of the number of incorrect guesses only track alphabetical letters
+	var incorrectGuesses = 8;
+	// - {1 entrynotcontained in wordInPlay};
+
+
+// display number of incorrect guesses
+	document.getElementById("incorrectGuesses")= incorrectGuesses;
+
+
+// when the max number of incorrect guesses has been reached tell the player they've lost and disable guessing
+
+
+
+// once all of the correct letters have been assigned tell the player they've won and disable guessing
+// # letters to be guessed in wordInPlay = 0, you win
